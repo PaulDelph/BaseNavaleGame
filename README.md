e628c11b18aba8ef51f71ef9889b6cfbfee23446
MIllimounoSoft <saapaulmillimouno@gmail.com>
2019-01-15 23:57:32 -0500

    Ma version Hello world

---
+++ README.md
@@ -0,0 +1,7 @@
+[user]
+    name = MIllimounoSoft
+    email = saapaulmillimouno@gmail.com
+[color]
+    ui = auto
+[core]
+    editor = nano # ou vim
---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main( int argv, char *argc[])
+{
+printf("\n Bonjour tous le monde j'apprend les base du langage C ! :) \n");
+ printf("voila! il me faudra beaucoup de pratique\n ");
+
+ 
+return 0;
+}
